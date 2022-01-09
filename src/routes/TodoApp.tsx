39320e872ed2751c5e908a556665f54833f510fc
Cezary Krawiec <web@cezarykrawiec.pl>
2022-01-09 21:03:43 +0100

    TODO_APP: add form structure

--- src/routes/TodoApp.tsx
+++ src/routes/TodoApp.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const TodoApp = () => {
+  const [task, setTask] = useState('');
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => setTask(e.target.value);
+
   return (
-    <h1 className="section-header">Todo App</h1> 
+    <div className="">
+      <h1 className="section-header">Todo App</h1> 
+      <form className="d-flex">
+        <input className="text-black mt-5 mr-2 p-2 rounded" type="text" value={task} onChange={handleInput}/>
+        <button className="bg-blue-500 hover:bg-blue-600 transition font-semibold py-2 px-5 rounded" type="submit">Add task</button>
+      </form>
+    </div>
   )
 }
-
 
 export default TodoApp;
