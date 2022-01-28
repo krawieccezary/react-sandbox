971a9d21cc10c69897ce76e3a7c88f2d5b8b217c
Cezary Krawiec <web@cezarykrawiec.pl>
2022-01-28 22:31:22 +0100

    Remove ReactPortal, MenuModal is a standalone

--- src/components/MenuBurger.tsx
+++ src/components/MenuBurger.tsx
@@ -8,7 +8,7 @@ type MenuBurgerProps = {
 const MenuBurger: FunctionComponent<MenuBurgerProps> = ({ setIsOpen, isOpen }) => {
 
   return (
-    <button className="flex w-10 h-10 flex-col absolute top-5 right-5 z-10" onClick={() => setIsOpen(!isOpen)}>
+    <button className="flex w-10 h-10 flex-col absolute top-5 right-5 z-20" onClick={() => setIsOpen(!isOpen)}>
       <span className="block bg-white w-10 h-2 rounded-md my-1"></span>
       <span className="block bg-white w-10 h-2 rounded-md my-1"></span>
       <span className="block bg-white w-10 h-2 rounded-md my-1"></span>
--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from 'react';
-import ReactDOM from 'react-dom';
 import { motion, AnimatePresence } from "framer-motion"
 
 import Nav from './Nav';
@@ -9,21 +8,20 @@ type MenuModalProps = {
   setIsOpen: Function,
 }
 
-const MenuModal: FunctionComponent<MenuModalProps>= ({ isOpen, setIsOpen }) => {
+const MenuModal: FunctionComponent<MenuModalProps> = ({ isOpen, setIsOpen }) => {
 
-  return ReactDOM.createPortal(
+  return (
     <AnimatePresence exitBeforeEnter>
       {isOpen && <motion.div 
-        className="flex bg-blue-900 h-screen w-2/5 justify-start items-center py-5 fixed top-0 right-0 px-32 pl-20"
+        className="flex bg-blue-900 h-screen w-2/5 justify-start items-center py-5 fixed top-0 right-0 px-32 pl-20 z-10"
         initial={{ x: '100%', opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: '100%', opacity: 0 }}
         transition={{ duration: .3 }}
       >
-        <Nav setIsOpen={setIsOpen} isOpen={isOpen} />
+        <Nav {...{isOpen, setIsOpen}} />
       </motion.div>}
-    </AnimatePresence>,
-    document.getElementById('menu-modal') as HTMLElement
+    </AnimatePresence>
   )
 }
 
